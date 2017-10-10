74c469c3c58e9d70509eae536665495ff4ac55f1
Lekealem Asong <lekealemasong@yahoo.de>
2017-10-10 14:38:32 +0200

    Use jasmine clock in async test

--- spec/async.spec.js
+++ spec/async.spec.js
@@ -1,23 +1,26 @@
 describe('async', function() {
-  it('should call a callback after 400ms with "Guten Tag"', function(done) {
+  beforeEach(() => jasmine.clock().install());
+  afterEach(() => jasmine.clock().uninstall());
+
+  it('should call a callback after 400ms with "Guten Tag"', function() {
     const inputValue = 'Guten Tag';
     const timoutValue = 400;
     let callback = function(value) {
       expect(value).toEqual(inputValue);
-      done();
     }
+    jasmine.clock().tick(timoutValue);
 
     asyncFunction(callback, inputValue, timoutValue);
   });
 
-  it('should call a callback using spies', function(done) {
+  it('should call a callback using spies', function() {
     const  inputValue = 'Foo Bar';
     const callback = jasmine.createSpy('007');
     callback.and.callFake((y) => {
       expect(y).toEqual(inputValue);
       expect(callback).toHaveBeenCalledWith(inputValue);
-      done();
     })
+    jasmine.clock().tick(400);
 
     asyncFunction(callback, inputValue, 400);
   });
